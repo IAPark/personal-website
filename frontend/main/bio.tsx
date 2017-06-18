8594bf166c5437e360577a7df299c42ab742856f
Isaac Park <isaac.a.park@gmail.com>
2017-06-18 12:42:25 -0400

    Styling cleanup

--- frontend/main/bio.tsx
+++ frontend/main/bio.tsx
@@ -20,54 +20,52 @@ interface Props {
 
 export function Bio (props: Props) {
     return  <div className="bio">
-                <div className="content">
-                    <div className='basic-info'>
-                        <h1>Basic</h1>
-                        <BasicInfoLine name='Name'>
-                            {props.name}
-                        </BasicInfoLine>
-                        <BasicInfoLine name='Age'>
-                            {Math.floor((new Date().getTime() - props.birth.getTime())/SECONDS_IN_A_YEAR)}
-                        </BasicInfoLine>
-                        <BasicInfoLine name='Emai'>
-                            <a href={'mailto:'+props.email}>{props.email}</a>
-                        </BasicInfoLine>
-                        <BasicInfoLine name='Phone'>
-                            <a href={'tell:'+props.phone}>{props.phone}</a>
-                        </BasicInfoLine>
-                        <h1>Languages</h1>
-                        <BasicInfoLine name='ES6 JS'>
-                            <Stars stars={5} filled={4}/>
-                        </BasicInfoLine>
-                        <BasicInfoLine name='Python'>
-                            <Stars stars={5} filled={4}/>
-                        </BasicInfoLine>
-                        <BasicInfoLine name='C++'>
-                            <Stars stars={5} filled={3.5}/>
-                        </BasicInfoLine>
-                        <BasicInfoLine name='Java'>
-                            <Stars stars={5} filled={3}/>
-                        </BasicInfoLine>
-                        <h1>Frameworks</h1>
-                        <BasicInfoLine name='React'>
-                            <Stars stars={5} filled={4}/>
-                        </BasicInfoLine>
-                        <BasicInfoLine name='Flask'>
-                            <Stars stars={5} filled={4}/>
-                        </BasicInfoLine>
-                        <BasicInfoLine name='Express'>
-                            <Stars stars={5} filled={2}/>
-                        </BasicInfoLine>
-                    </div>
+                <div className='basic-info'>
+                    <h1>Basic</h1>
+                    <BasicInfoLine name='Name'>
+                        {props.name}
+                    </BasicInfoLine>
+                    <BasicInfoLine name='Age'>
+                        {Math.floor((new Date().getTime() - props.birth.getTime())/SECONDS_IN_A_YEAR)}
+                    </BasicInfoLine>
+                    <BasicInfoLine name='Emai'>
+                        <a href={'mailto:'+props.email}>{props.email}</a>
+                    </BasicInfoLine>
+                    <BasicInfoLine name='Phone'>
+                        <a href={'tell:'+props.phone}>{props.phone}</a>
+                    </BasicInfoLine>
+                    <h1>Languages</h1>
+                    <BasicInfoLine name='ES6 JS'>
+                        <Stars stars={5} filled={4}/>
+                    </BasicInfoLine>
+                    <BasicInfoLine name='Python'>
+                        <Stars stars={5} filled={4}/>
+                    </BasicInfoLine>
+                    <BasicInfoLine name='C++'>
+                        <Stars stars={5} filled={3.5}/>
+                    </BasicInfoLine>
+                    <BasicInfoLine name='Java'>
+                        <Stars stars={5} filled={3}/>
+                    </BasicInfoLine>
+                    <h1>Frameworks</h1>
+                    <BasicInfoLine name='React'>
+                        <Stars stars={5} filled={4}/>
+                    </BasicInfoLine>
+                    <BasicInfoLine name='Flask'>
+                        <Stars stars={5} filled={4}/>
+                    </BasicInfoLine>
+                    <BasicInfoLine name='Express'>
+                        <Stars stars={5} filled={2}/>
+                    </BasicInfoLine>
+                </div>
 
-                    
-                    <div className="description">
-                        <div className="pic">
-                            <img src={props.photo}/>
-                        </div>
-                        <h1>About Me</h1>
-                        {props.children}
+                
+                <div className="description">
+                    <div className="pic">
+                        <img src={props.photo}/>
                     </div>
+                    <h1>About Me</h1>
+                    {props.children}
                 </div>
             </div>
 }
