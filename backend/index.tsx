44659ded62a68b49a18ce13b243f67953c509869
Isaac Park <isaac.a.park@gmail.com>
2017-06-26 23:10:54 -0400

    Fix includes

--- backend/index.tsx
+++ backend/index.tsx
@@ -10,19 +10,23 @@ app.get('/', function (req, res) {
   res.send(renderToStaticMarkup(
     <html>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
         <title>Isaac Park</title>
+
+        <link rel="stylesheet" href="/public/style.css"/>
       </head>
       <body>
         <div id='content' dangerouslySetInnerHTML={{__html: renderToString(<MainPage/>)}}/>
         <script src={req.protocol+"://cdnjs.cloudflare.com/ajax/libs/react/15.4.2/react.min.js"}/>
         <script src={req.protocol+"://cdnjs.cloudflare.com/ajax/libs/react/15.4.2/react-dom.min.js"}/>
-        <script src={"bundle.js"}/>
+        <script src="/public/bundle.js"/>
       </body>
     </html>
   ));
 });
 
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
