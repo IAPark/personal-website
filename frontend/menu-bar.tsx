308864041a720b881d471d01447d7f7a9aadb166
Isaac Park <isaac.a.park@gmail.com>
2017-07-01 15:52:36 -0400

    Link to resume

--- frontend/menu-bar.tsx
+++ frontend/menu-bar.tsx
@@ -10,9 +10,9 @@ export function MenuBar (props: Props) {
                 <IconLink icon='github' href='http://github.com/iapark'/>
                 <IconLink icon='linkedin' href='https://www.linkedin.com/in/isaac-park-a95280a3/'/>
                 <IconLink icon='facebook'  href='https://www.facebook.com/isaac.a.park'/>
+                <IconLink icon='resume' href='/public/resume.pdf'/>
                 <IconLink icon='globe' href='http://old.isaacpark.me/'>
                     Old Site
                 </IconLink>
-
             </div>
 }
